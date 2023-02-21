b5729b9b629143c4581b7cc600c208810cadaf27
Rahat393 <rahataminulislam37@gmail.com>
2023-02-21 05:53:01 -0800

    about added

---
+++ src/Pages/Shared/Home/About/About.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import person from '../../../../assets/images/about_us/person.jpg';
+import parts from '../../../../assets/images/about_us/parts.jpg'
+import Button from '../../../../Components/Button/Button';
+
+const About = () => {
+  return (
+    <div className="hero  ">
+    <div className="hero-content flex-col lg:flex-row">
+        <div className='relative w-1/2'>
+            <img src={person} alt="" className="w-4/5 h-full rounded-lg shadow-2xl" />
+            <img src={parts} alt="" className="absolute right-5 top-1/2 w-3/5 border-8 border-white rounded-lg shadow-2xl" />
+        </div>
+        <div className='w-1/2'>
+            <p className="text-4xl font-bold text-[#FF3811]">About Us</p>
+            <h1 className="my-5 text-5xl font-bold">
+                We are qualified <br />
+                & of experience <br />
+                in this field</h1>
+            <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+            <p className="py-6">the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+            <Button>Get More Info</Button>
+        </div>
+    </div>
+</div>
+  );
+};
+
+export default About;
